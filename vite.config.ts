8af740a1c8b44921863412ef64241cd6b6190465
MokahMokah <107811831+Loomiquu@users.noreply.github.com>
2023-12-06 23:00:55 -0600

    fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import solid from "solid-start/vite";
+import solidStatic from "solid-start-static";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({ ssr: false })],
+  base: "/my-project/",
+  // insert your github project name between slashes above
+  plugins: [solid({ adapter: solidStatic() })],
 });
